d7464f66e3179786a6ce33fae02f62a1df0d631d
Linda Andersson <lindaandersson@Lindas-MacBook-Pro.local>
2021-01-19 21:26:43 +0100

    more styling

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,16 +4,28 @@
       <div class="left col-sm-12 col-md-6 col-lg-6 d-flex p-0 m-0">
         <div class="contact m-0 ">
          <div class="uppercase">Say Hi!</div>
-        </div>
-       
+        </div>  
       </div>
-      <div class="styling-box"></div>
 
       <div class="right col-sm-12 col-md-6 col-lg-6">
         <div class="contact-me">
-          <div class="contact"> <span class="material-icons">call</span> 076-8222455</div>
-          <div class="contact"> <span class="material-icons">room</span> Stockholm</div>
-          <div class="contact"><span class="material-icons">email</span> Linda.andersson@Medinstitutet.se</div> 
+          <div class="contact"> 
+            <span class="material-icons">call</span>
+           076-8222455
+          </div>
+
+          <div class="contact">
+            <span class="material-icons">room</span> 
+            Stockholm
+          </div>
+          <div class="contact">
+            <a class="send-mail" href="mailto:linda.andersson@medieinstitutet.se">
+            <span class="material-icons">
+            email
+            </span> 
+            Linda.andersson@medinstitutet.se
+            </a>
+          </div>
         </div>
 
 
@@ -33,22 +45,17 @@ export default {
 <style lang="scss" scoped>
 
 .background {
-    // background-image: url('../assets/img/linda.jpeg');
     background-color: rgba(#919891, 0.8);
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
     height: 100vh;
     width: 100%;
     
       .left{
-       
 
         .contact{
           font-family: 'Prata', serif;
           font-size: 400;
           padding-top:300px;  
-          padding-left: 30%;
+          padding-left: 25%;
           color:black;
           font-size: 60px;
           font-weight: 200;
@@ -60,7 +67,7 @@ export default {
                
             }
             @include large {
-              padding-top: 300px;
+              padding-top: 180px;
               padding-left: 40%;
                
             }
@@ -69,62 +76,47 @@ export default {
       }
       
       .right{
+        padding: 0;
          .contact-me{
-           font-size: 25px;
-           .uppercase
-           {
-            text-transform: uppercase;
-            padding: 20px 0px 10px 0px;
+            color:black;
+            padding-left: 25%;
+            font-size: 15px;
+            padding-top: 10px;
+            justify-content: center;
+
+            @include medium {
+            padding-top:500px;
+            padding-left: 0%;
+            font-size: 18px;
+               
+            }
+
+            @include large {
+            padding-top:260px;  
+               
+            }
+            .material-icons{
+              font-size: 20px;
+              padding: 10px 0 0 0;
+
+            @include medium{
+              font-size: 30px;
+            }
+            }
+
+           a{
+             color: black;
+             cursor: pointer;
+
+             & :hover{
+               text-decoration: underline;
+             }
+             
            }
-          
-          color:black;
-          padding-left: 30%;
-          
 
-          .contact{
-            margin-top: 10px;
-            font-size: 12px;
-
-          }
-
-         @include medium {
-           padding-top:500px;  
-               
-            }
-         @include large {
-           padding-top:260px;  
-               
-            }
-
-            
          }
       }
-
-      .styling-box{
-
-
-            // @include medium {
-            //   border-right:white solid 1px;
-            //   border-bottom: white solid 1px;
-            //   position: absolute;
-            //   margin: 20px 0px 0px 70px;
-            //   width: 500px;
-            //   height: 500px;
-               
-            // }
-            // @include large {
-            //   width: 600px;
-            //   height: 400px;
-               
-            // }
-
-            
-       
-     
-
-      }
-
-    
+ 
 }
 
 </style>
